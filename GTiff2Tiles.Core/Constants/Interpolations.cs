46fb72b7d0c5c778b47bf83da6e4ddee965f3da2
Gigas002 <gigas002@yandex.ru>
2020-07-21 20:29:28 +0300

    Rewritten Interpolations

---
+++ GTiff2Tiles.Core/Constants/Interpolations.cs
@@ -0,0 +1,21 @@
+﻿namespace GTiff2Tiles.Core.Constants
+{
+    /// <summary>
+    /// Represents the interpolation algorithms
+    /// </summary>
+    public static class Interpolations
+    {
+        /// <summary>Nearest-neighbour interpolation.</summary>
+        public const string Nearest = "nearest";
+        /// <summary>Linear interpolation.</summary>
+        public const string Linear = "linear";
+        /// <summary>Cubic interpolation.</summary>
+        public const string Cubic = "cubic";
+        /// <summary>Mitchell</summary>
+        public const string Mitchell = "mitchell";
+        /// <summary>Two-lobe Lanczos.</summary>
+        public const string Lanczos2 = "lanczos2";
+        /// <summary>Three-lobe Lanczos.</summary>
+        public const string Lanczos3 = "lanczos3";
+    }
+}
