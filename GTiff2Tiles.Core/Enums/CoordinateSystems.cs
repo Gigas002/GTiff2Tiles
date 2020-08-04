b839571583eb244bacb043312839efe35eb5b871
Gigas002 <gigas002@yandex.ru>
2020-08-04 17:34:15 +0300

    Add CoordinateSystems enum

---
+++ GTiff2Tiles.Core/Enums/CoordinateSystems.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+// ReSharper disable UnusedMember.Global
+
+namespace GTiff2Tiles.Core.Enums
+{
+    /// <summary>
+    /// Supported EPSG coordinate systems
+    /// </summary>
+    [Flags]
+    public enum CoordinateSystems
+    {
+        /// <summary>
+        /// EPSG:4326
+        /// </summary>
+        Epsg4326 = 0,
+
+        /// <summary>
+        /// EPSG:3857
+        /// </summary>
+        Epsg3857 = 1,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg3587 = Epsg3857,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg3785 = Epsg3857,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg41001 = Epsg3857,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg54004 = Epsg3857,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg102113 = Epsg3857,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg102100 = Epsg3857,
+
+        /// <summary>
+        /// Replaced by <see cref="Epsg3857"/>
+        /// </summary>
+        [Obsolete("Replaced by EPSG:3857")]
+        Epsg900913 = Epsg3857
+    }
+}
