c443befc36b2c3fd66fef65dd24f26039d78c7e5
Gigas002 <gigas002@yandex.ru>
2020-08-11 19:04:50 +0300

    Add exceptions tests

---
+++ GTiff2Tiles.Tests/Tests/Exceptions/ExceptionsTests.cs
@@ -0,0 +1,33 @@
+﻿using GTiff2Tiles.Core.Exceptions;
+using NUnit.Framework;
+
+namespace GTiff2Tiles.Tests.Tests.Exceptions
+{
+    [TestFixture]
+    public sealed class ExceptionsTests
+    {
+        [Test]
+        public void RasterExceptionConstructors() => Assert.DoesNotThrow(() =>
+        {
+            RasterException e1 = new RasterException();
+            RasterException e2 = new RasterException(string.Empty);
+            RasterException e3 = new RasterException(string.Empty, null);
+        });
+
+        [Test]
+        public void FileExceptionConstructors() => Assert.DoesNotThrow(() =>
+        {
+            FileException e1 = new FileException();
+            FileException e2 = new FileException(string.Empty);
+            FileException e3 = new FileException(string.Empty, null);
+        });
+
+        [Test]
+        public void DirectoryExceptionConstructors() => Assert.DoesNotThrow(() =>
+        {
+            DirectoryException e1 = new DirectoryException();
+            DirectoryException e2 = new DirectoryException(string.Empty);
+            DirectoryException e3 = new DirectoryException(string.Empty, null);
+        });
+    }
+}
---
+++ GTiff2Tiles.Tests/Tests/GeoTiffs/GeoTiffTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace GTiff2Tiles.Tests.Tests.GeoTiffs
+{
+    [TestFixture]
+    public sealed class GeoTiffTests
+    {
+        // TODO
+    }
+}
