8722be9216982c8e24bde057731a983c730076ba
Gigas002 <gigas002@yandex.ru>
2020-08-11 19:24:03 +0300

    Add enums tests

---
+++ GTiff2Tiles.Tests/Tests/Enums/EnumsTests.cs
@@ -0,0 +1,43 @@
+﻿using GTiff2Tiles.Core.Enums;
+using NUnit.Framework;
+
+namespace GTiff2Tiles.Tests.Tests.Enums
+{
+    [TestFixture]
+    public sealed class EnumsTests
+    {
+        [Test]
+        public void GetTileExtensions() => Assert.DoesNotThrow(() =>
+        {
+            TileExtension e = TileExtension.Png;
+            e = TileExtension.Jpg;
+            e = TileExtension.Webp;
+        });
+
+        [Test]
+        public void GetInterpolations() => Assert.DoesNotThrow(() =>
+        {
+            Interpolation i = Interpolation.Nearest;
+            i = Interpolation.Linear;
+            i = Interpolation.Cubic;
+            i = Interpolation.Mitchell;
+            i = Interpolation.Lanczos2;
+            i = Interpolation.Lanczos3;
+        });
+
+        [Test]
+        public void GetCoordinateSystems() => Assert.DoesNotThrow(() =>
+        {
+            CoordinateSystem cs = CoordinateSystem.Epsg4326;
+            cs = CoordinateSystem.Epsg3857;
+            cs = CoordinateSystem.Epsg3587;
+            cs = CoordinateSystem.Epsg3785;
+            cs = CoordinateSystem.Epsg41001;
+            cs = CoordinateSystem.Epsg54004;
+            cs = CoordinateSystem.Epsg102113;
+            cs = CoordinateSystem.Epsg102100;
+            cs = CoordinateSystem.Epsg900913;
+            cs = CoordinateSystem.Other;
+        });
+    }
+}
--- GTiff2Tiles.Tests/Tests/Exceptions/ExceptionsTests.cs
+++ GTiff2Tiles.Tests/Tests/Exceptions/ExceptionsTests.cs
@@ -9,25 +9,25 @@ namespace GTiff2Tiles.Tests.Tests.Exceptions
         [Test]
         public void RasterExceptionConstructors() => Assert.DoesNotThrow(() =>
         {
-            RasterException e1 = new RasterException();
-            RasterException e2 = new RasterException(string.Empty);
-            RasterException e3 = new RasterException(string.Empty, null);
+            RasterException e = new RasterException();
+            e = new RasterException(string.Empty);
+            e = new RasterException(string.Empty, null);
         });
 
         [Test]
         public void FileExceptionConstructors() => Assert.DoesNotThrow(() =>
         {
-            FileException e1 = new FileException();
-            FileException e2 = new FileException(string.Empty);
-            FileException e3 = new FileException(string.Empty, null);
+            FileException e = new FileException();
+            e = new FileException(string.Empty);
+            e = new FileException(string.Empty, null);
         });
 
         [Test]
         public void DirectoryExceptionConstructors() => Assert.DoesNotThrow(() =>
         {
-            DirectoryException e1 = new DirectoryException();
-            DirectoryException e2 = new DirectoryException(string.Empty);
-            DirectoryException e3 = new DirectoryException(string.Empty, null);
+            DirectoryException e = new DirectoryException();
+            e = new DirectoryException(string.Empty);
+            e = new DirectoryException(string.Empty, null);
         });
     }
 }
