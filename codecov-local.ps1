127266c2a9321ef958d667d88c02275b91804130
Gigas002 <gigas002@yandex.ru>
2020-08-15 14:21:41 +0300

    WIP on codecov

--- codecov-local.ps1
+++ codecov-local.ps1
@@ -1,4 +1,11 @@
 $token=$args[0]
 
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Platform=x64 -c Release
+# WARNING! Using -c Release crashes coverlet results!
+
+# For coverlet.msbuild
+# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Platform=x64 /p:Exclude="[GTiff2Tiles.*]GTiff2Tiles.Core.Localization.*" /p:Include="[GTiff2Tiles.Core]*"
+
+# For coverlet.collector
+dotnet test --collect:"XPlat Code Coverage" --settings runsettings.xml /p:Platform=x64
+
 # codecov -f "GTiff2Tiles.Tests/coverage.opencover.xml" -t $token
