a5478d885a16d73e8efdcad4ded5ce439e77a23e
Gigas002 <gigas002@yandex.ru>
2020-08-15 16:09:34 +0300

    Update codcov-local script

--- codecov-local.ps1
+++ codecov-local.ps1
@@ -3,9 +3,9 @@ $token=$args[0]
 # WARNING! Using -c Release crashes coverlet results!
 
 # For coverlet.msbuild
-# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Platform=x64 /p:Exclude="[GTiff2Tiles.*]GTiff2Tiles.Core.Localization.*" /p:Include="[GTiff2Tiles.Core]*"
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Platform=x64 /p:Exclude="[GTiff2Tiles.*]GTiff2Tiles.Core.Localization.*" /p:Include="[GTiff2Tiles.Core]*"
 
 # For coverlet.collector
-dotnet test --collect:"XPlat Code Coverage" --settings runsettings.xml /p:Platform=x64
+# dotnet test --collect:"XPlat Code Coverage" --settings runsettings.xml /p:Platform=x64
 
-# codecov -f "GTiff2Tiles.Tests/coverage.opencover.xml" -t $token
+codecov -f "GTiff2Tiles.Tests/coverage.opencover.xml" -t $token
