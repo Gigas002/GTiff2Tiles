80598b35d6c9621bac9763e882ba9c3bada5eeab
Gigas002 <gigas002@yandex.ru>
2020-11-18 23:00:53 +0300

    Should have fixed appveyor deploy, closes #86

--- publish-github-release.ps1
+++ publish-github-release.ps1
@@ -1,16 +1,4 @@
-$isAppveyor=$args[0]
-$appveyorWorker=$args[1]
-
-# TODO: move to vs publishing?
-# TODO: publish profiles needs changes
-
-$appVsProfile="Visual Studio 2019"
-
-if ($appveyorWorker -eq $appVsProfile)
-{
-    Write-Output "Using VS2019, skip publishing github release..."
-    return
-}
+# Publishes all but not optimal version for all OS
 
 Write-Output "Started building/publishing"
 
@@ -20,68 +8,40 @@ Write-Output "Start building GTiff2Tiles.Core"
 dotnet publish "GTiff2Tiles.Core/GTiff2Tiles.Core.csproj" -c Release /p:Platform=x64
 Write-Output "Ended building GTiff2Tiles.Core"
 
-# GTiff2Tiles.Benhmarks
-
-# if (!$isAppveyor)
-# {
-#     # Win-x64
-#     Write-Output "Start Win-x64-Benchmarks publish"
-#     if ($IsWindows)
-#     {
-#         dotnet publish "GTiff2Tiles.Benchmarks/GTiff2Tiles.Benchmarks.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Benchmarks/win-x64 /p:IncludeAllContentInSingleFile=true /p:PublishReadyToRun=true # /p:PublishSingleFile=true /p:PublishTrimmed=true
-#     }
-#     else
-#     {
-#         dotnet publish "GTiff2Tiles.Benchmarks/GTiff2Tiles.Benchmarks.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Benchmarks/win-x64 /p:IncludeAllContentInSingleFile=true # /p:PublishSingleFile=true /p:PublishTrimmed=true
-#     }
-#     Write-Output "Ended Win-x64-Benchmarks publish"
-
-#     # Linux-x64
-#     Write-Output "Start Linux-x64-Benchmarks publish"
-#     if ($IsWindows)
-#     {
-#         dotnet publish "GTiff2Tiles.Benchmarks/GTiff2Tiles.Benchmarks.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Benchmarks/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true
-#     }
-#     else
-#     {
-#         dotnet publish "GTiff2Tiles.Benchmarks/GTiff2Tiles.Benchmarks.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Benchmarks/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true
-#     }
-#     Write-Output "Ended Linux-x64-Benchmarks publish"
-# }
-
 # GTiff2Tiles.Console
 
-# Win-x64
-Write-Output "Start Win-x64-Console publish"
+# win-x64
+Write-Output "Start win-x64-Console publish"
 if ($IsWindows)
 {
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Console/win-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true
+    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Console/win-x64 /p:PublishReadyToRun=true
 }
 else
 {
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Console/win-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true
+    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Console/win-x64
 }
-Write-Output "Ended Win-x64-Console publish"
+Write-Output "Ended win-x64-Console publish"
 
-# Linux-x64
-Write-Output "Start Linux-x64-Console publish"
+# linux-x64
+Write-Output "Start linux-x64-Console publish"
 if ($IsWindows)
 {
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true
+    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64
 }
 else
 {
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true
+    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64 /p:PublishReadyToRun=true
 }
-Write-Output "Ended Linux-x64-Console publish"
+Write-Output "Ended linux-x64-Console publish"
 
 # GTiff2Tiles.GUI
+
 if ($IsWindows)
 {
-    # Win-x64
-    Write-Output "Start Win-x64-GUI publish"
+    # win-x64
+    Write-Output "Start win-x64-GUI publish"
     dotnet publish "GTiff2Tiles.GUI/GTiff2Tiles.GUI.csproj" -c Release -o Publish/GTiff2Tiles.GUI/win-x64
-    Write-Output "Ended Win-x64-GUI publish"
+    Write-Output "Ended win-x64-GUI publish"
 }
 
 Write-Output "Ended building/publishing"
@@ -93,16 +53,7 @@ Get-ChildItem "Publish/" -Include *.pdb -Recurse | Remove-Item
 # Copy docs, etc to published directories before zipping them
 Write-Output "Copying docs, LICENSE.md, etc to published directories"
 
-# if (!$isAppveyor)
-# {
-#     Copy-Item -Path "GTiff2Tiles.Benchmarks/benchmarks-index.pdf" -Destination "Publish/GTiff2Tiles.Benchmarks/win-x64/benchmarks-index.pdf"
-#     Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Benchmarks/win-x64/LICENSE.md"
-#     Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Benchmarks/win-x64/CHANGELOG.md"
-
-#     Copy-Item -Path "GTiff2Tiles.Benchmarks/benchmarks-index.pdf" -Destination "Publish/GTiff2Tiles.Benchmarks/linux-x64/benchmarks-index.pdf"
-#     Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Benchmarks/linux-x64/LICENSE.md"
-#     Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Benchmarks/linux-x64/CHANGELOG.md"
-# }
+# Console
 
 Copy-Item -Path "GTiff2Tiles.Console/console-index.pdf" -Destination "Publish/GTiff2Tiles.Console/win-x64/console-index.pdf"
 Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Console/win-x64/LICENSE.md"
@@ -111,6 +62,8 @@ Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Console/win-x64
 Copy-Item -Path "GTiff2Tiles.Console/console-index.pdf" -Destination "Publish/GTiff2Tiles.Console/linux-x64/console-index.pdf"
 Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Console/linux-x64/LICENSE.md"
 Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Console/linux-x64/CHANGELOG.md"
+
+# GUI
 
 if ($IsWindows)
 {
@@ -122,22 +75,22 @@ if ($IsWindows)
 
 Write-Output "Started adding published files to .zip archives"
 
-# Add Win-x64-GUI to .zip
+# Add win-x64-Console to .zip
+Write-Output "Start adding win-x64-Console to .zip"
+Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../win-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/win-x64/"
+Write-Output "Ended adding win-x64-Console to .zip"
+
+# Add linux-x64-Console to .zip
+Write-Output "Start adding linux-x64-Console to .zip"
+Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../linux-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/linux-x64/"
+Write-Output "Ended adding linux-x64-Console to .zip"
+
+# Add win-x64-GUI to .zip
 if ($IsWindows)
 {
-    Write-Output "Start adding Win-x64-GUI to .zip"
-    Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../Win-x64-GTiff2Tiles.GUI.zip *" -WorkingDirectory "Publish/GTiff2Tiles.GUI/win-x64/" | Out-Null
-    Write-Output "Ended adding Win-x64-GUI to .zip"
+    Write-Output "Start adding win-x64-GUI to .zip"
+    Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../win-x64-GTiff2Tiles.GUI.zip *" -WorkingDirectory "Publish/GTiff2Tiles.GUI/win-x64/"
+    Write-Output "Ended adding win-x64-GUI to .zip"
 }
-
-# Add Win-x64-Console to .zip
-Write-Output "Start adding Win-x64-Console to .zip"
-Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../Win-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/win-x64/"
-Write-Output "Ended adding Win-x64-Console to .zip"
-
-# Add Linux-x64-Console to .zip
-Write-Output "Start adding Linux-x64-Console to .zip"
-Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../Linux-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/linux-x64/"
-Write-Output "Ended adding Linux-x64-Console to .zip"
 
 Write-Output "Ended adding published files to .zip archives"
