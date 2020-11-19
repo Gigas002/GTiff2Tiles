9d4be0bc21f500ebe8680a1eabb64c477dcd0f8d
Gigas002 <gigas002@yandex.ru>
2020-11-19 17:48:39 +0300

    Simplify publish script

--- publish-github-release.ps1
+++ publish-github-release.ps1
@@ -1,4 +1,4 @@
-# Publishes all but not optimal version for all OS
+$buildAll=$args[0]
 
 Write-Output "Started building/publishing"
 
@@ -8,40 +8,27 @@ Write-Output "Start building GTiff2Tiles.Core"
 dotnet publish "GTiff2Tiles.Core/GTiff2Tiles.Core.csproj" -c Release /p:Platform=x64
 Write-Output "Ended building GTiff2Tiles.Core"
 
-# GTiff2Tiles.Console
 
 # win-x64
-Write-Output "Start win-x64-Console publish"
-if ($IsWindows)
+if ($IsWindows -or $buildAll)
 {
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Console/win-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true
-}
-else
-{
+    # GTiff2Tiles.Console
+    Write-Output "Start win-x64-Console publish"
     dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r win-x64 -o Publish/GTiff2Tiles.Console/win-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true
-}
-Write-Output "Ended win-x64-Console publish"
+    Write-Output "Ended win-x64-Console publish"
 
-# linux-x64
-Write-Output "Start linux-x64-Console publish"
-if ($IsWindows)
-{
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true
-}
-else
-{
-    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true
-}
-Write-Output "Ended linux-x64-Console publish"
-
-# GTiff2Tiles.GUI
-
-if ($IsWindows)
-{
-    # win-x64
+    # GTiff2Tiles.GUI
     Write-Output "Start win-x64-GUI publish"
     dotnet publish "GTiff2Tiles.GUI/GTiff2Tiles.GUI.csproj" -c Release -o Publish/GTiff2Tiles.GUI/win-x64
     Write-Output "Ended win-x64-GUI publish"
+}
+
+# linux-x64
+if ($IsLinux -or $buildAll)
+{
+    Write-Output "Start linux-x64-Console publish"
+    dotnet publish "GTiff2Tiles.Console/GTiff2Tiles.Console.csproj" -c Release -r linux-x64 -o Publish/GTiff2Tiles.Console/linux-x64 /p:IncludeAllContentInSingleFile=true /p:PublishSingleFile=true /p:PublishTrimmed=true
+    Write-Output "Ended linux-x64-Console publish"
 }
 
 Write-Output "Ended building/publishing"
@@ -53,44 +40,52 @@ Get-ChildItem "Publish/" -Include *.pdb -Recurse | Remove-Item
 # Copy docs, etc to published directories before zipping them
 Write-Output "Copying docs, LICENSE.md, etc to published directories"
 
-# Console
-
-Copy-Item -Path "GTiff2Tiles.Console/console-index.pdf" -Destination "Publish/GTiff2Tiles.Console/win-x64/console-index.pdf"
-Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Console/win-x64/LICENSE.md"
-Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Console/win-x64/CHANGELOG.md"
-
-Copy-Item -Path "GTiff2Tiles.Console/console-index.pdf" -Destination "Publish/GTiff2Tiles.Console/linux-x64/console-index.pdf"
-Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Console/linux-x64/LICENSE.md"
-Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Console/linux-x64/CHANGELOG.md"
-
-# GUI
-
-if ($IsWindows)
+# win-x64
+if ($IsWindows -or $buildAll)
 {
+    # GTiff2Tiles.Console
+    Copy-Item -Path "GTiff2Tiles.Console/console-index.pdf" -Destination "Publish/GTiff2Tiles.Console/win-x64/console-index.pdf"
+    Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Console/win-x64/LICENSE.md"
+    Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Console/win-x64/CHANGELOG.md"
+
+    # GTiff2Tiles.GUI
     Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.GUI/win-x64/LICENSE.md"
     Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.GUI/win-x64/CHANGELOG.md"
+}
+
+# linux-x64
+if ($IsLinux -or $buildAll)
+{
+    # linux-x64-console
+    Copy-Item -Path "GTiff2Tiles.Console/console-index.pdf" -Destination "Publish/GTiff2Tiles.Console/linux-x64/console-index.pdf"
+    Copy-Item -Path "LICENSE.md" -Destination "Publish/GTiff2Tiles.Console/linux-x64/LICENSE.md"
+    Copy-Item -Path "CHANGELOG.md" -Destination "Publish/GTiff2Tiles.Console/linux-x64/CHANGELOG.md"
 }
 
 # Add everything into archives. Requires installed 7z added to PATH.
 
 Write-Output "Started adding published files to .zip archives"
 
-# Add win-x64-Console to .zip
-Write-Output "Start adding win-x64-Console to .zip"
-Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../win-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/win-x64/"
-Write-Output "Ended adding win-x64-Console to .zip"
-
-# Add linux-x64-Console to .zip
-Write-Output "Start adding linux-x64-Console to .zip"
-Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../linux-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/linux-x64/"
-Write-Output "Ended adding linux-x64-Console to .zip"
-
-# Add win-x64-GUI to .zip
-if ($IsWindows)
+# win-x64
+if ($IsWindows -or $buildAll)
 {
+    # GTiff2Tiles.Console
+    Write-Output "Start adding win-x64-Console to .zip"
+    Start-Process -NoNewwindow -Wait -FilePath "7z" -ArgumentList "a ../../win-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/win-x64/"
+    Write-Output "Ended adding win-x64-Console to .zip"
+
+    # GTiff2Tiles.GUI
     Write-Output "Start adding win-x64-GUI to .zip"
-    Start-Process -NoNewWindow -Wait -FilePath "7z" -ArgumentList "a ../../win-x64-GTiff2Tiles.GUI.zip *" -WorkingDirectory "Publish/GTiff2Tiles.GUI/win-x64/"
+    Start-Process -NoNewwindow -Wait -FilePath "7z" -ArgumentList "a ../../win-x64-GTiff2Tiles.GUI.zip *" -WorkingDirectory "Publish/GTiff2Tiles.GUI/win-x64/"
     Write-Output "Ended adding win-x64-GUI to .zip"
+}
+
+# linux-x64
+if ($IsLinux -or $buildAll)
+{
+    Write-Output "Start adding linux-x64-Console to .zip"
+    Start-Process -NoNewwindow -Wait -FilePath "7z" -ArgumentList "a ../../linux-x64-GTiff2Tiles.Console.zip *" -WorkingDirectory "Publish/GTiff2Tiles.Console/linux-x64/"
+    Write-Output "Ended adding linux-x64-Console to .zip"
 }
 
 Write-Output "Ended adding published files to .zip archives"
